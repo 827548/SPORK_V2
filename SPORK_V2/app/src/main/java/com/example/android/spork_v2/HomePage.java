bc516a5482346b338ad2999f5c17410484b4c5ce
Jeremiah <827548@pinelakeprep.org>
2017-12-18 10:16:16 -0500

    fix my life
    off

---
+++ SPORK_V2/app/src/main/java/com/example/android/spork_v2/HomePage.java
@@ -0,0 +1,13 @@
+package com.example.android.spork_v2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
