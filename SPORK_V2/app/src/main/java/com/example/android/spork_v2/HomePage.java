6ad249b7366a9abf400ffc119b40730ca8e491a1
JoshuaRichardson45 <8386376791@plps-8386376791.pinelakeprep.org>
2017-12-19 09:42:26 -0500

    dank

--- SPORK_V2/app/src/main/java/com/example/android/spork_v2/HomePage.java
+++ SPORK_V2/app/src/main/java/com/example/android/spork_v2/HomePage.java
@@ -1,13 +1,18 @@
 package com.example.android.spork_v2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
+
+
 public class HomePage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
     }
 }
