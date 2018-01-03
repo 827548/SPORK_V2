43fca9625be299e917625d513a7eaa9bddebdcd3
Jeremiah <827548@pinelakeprep.org>
2018-01-03 10:11:58 -0500

    Revert "ok"
    This reverts commit ce4863ddfb66647519d7cc536c5bd2bca5becad3.

--- SPORK_V2/app/src/main/java/com/example/android/spork_v2/LoginPage.java
+++ SPORK_V2/app/src/main/java/com/example/android/spork_v2/LoginPage.java
@@ -1,32 +1,13 @@
 package com.example.android.spork_v2;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
-
-    Button btnLogin;
-    CardView btnCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-
-        btnLogin = (Button) findViewById(R.id.LoginButtonText);
-        btnCard = (CardView) findViewById(R.id.LoginButton);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                btnLogin.setBackgroundColor(14040064);
-
-            }
-        });
     }
 }
