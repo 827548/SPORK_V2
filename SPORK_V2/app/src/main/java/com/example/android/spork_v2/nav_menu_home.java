0afa2aa49139cf2c575285d371f97c631a01929b
JoshuaRichardson45 <8386376791@plps-8386376791.pinelakeprep.org>
2018-01-12 09:48:18 -0500

    nav menu stuffs

--- SPORK_V2/app/src/main/java/com/example/android/spork_v2/nav_menu_home.java
+++ SPORK_V2/app/src/main/java/com/example/android/spork_v2/nav_menu_home.java
@@ -90,7 +90,7 @@ public class nav_menu_home extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_view) {
 
         }
 
